c0eee909b603d491ccaac0be6d1cc036ce8dd956
Evan Brown <ebrown17@mail.csuchico.edu>
2019-07-06 17:25:24 -0700

    have moshi working how I think I want it

--- src/main/kotlin/konem/Main.kt
+++ src/main/kotlin/konem/Main.kt
@@ -5,8 +5,7 @@ import kotlinx.coroutines.*
 import org.slf4j.LoggerFactory
 import java.net.InetSocketAddress
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import konem.protocol.websocket.KoneMessageReceiver
-import konem.protocol.websocket.KoneMesssage
+import konem.protocol.websocket.*
 import java.util.*
 
 
@@ -16,79 +15,91 @@ private val scopey = CoroutineScope(cName)
 
 
 fun main() {
-    logger.info("hello from main")
+  logger.info("hello from main")
 
-    val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+  val moshi =
+    Moshi.Builder().add(MainMessageAdaptor()).add(KotlinJsonAdapterFactory()).build()
 
-    val adapter = moshi.adapter(KoneMesssage::class.java)
+  val adapter = moshi.adapter(MainMessage::class.java)
 
-    var receiver = KoneMessageReceiver { remote, message ->
-        logger.info("KoneMessageReceiver: {} ", message)
-    }
+  var receiver = KoneMessageReceiver { remote, message ->
+    logger.info("KoneMessageReceiver: {} ", message)
+  }
 
-    val testo = KoneMesssage("heartbeat", Date().toString())
-    println(testo)
-    var jjson = adapter.toJson(testo)
+  val heartb = MainMessage.Heartbeat()
+  var jheartb = adapter.toJson(heartb)
+  logger.info("heartb before json: {}", heartb)
+  logger.info("jheartb json string: {}", jheartb)
+  val heartBack = adapter.fromJson(jheartb)
+  logger.info("jheartb converted back to heartbxxx: {}", heartBack)
 
-    logger.info(" jjson dmsg: {}", testo)
-    logger.info(" jjson string: {}", jjson)
-    receiver.handleChannelRead(InetSocketAddress(8080), jjson)
+  //println("${heartBack?.type} + ${ttt}")
+
+
+/*
+  val statusb = MainMessage.Status("GOOD",0,5000,0,"GOOD")
+  var jstatusb = adapter.toJson(statusb)
+  logger.info("statusb before json: {}", statusb)
+  logger.info("statusb json string: {}", jstatusb)
+*/
+
+  // receiver.handleChannelRead(InetSocketAddress(8080), jjson)
 
 /*    runBlocking { tester() }*/
-    // onConnection()
-    //val job =tester2()
-    logger.info("after")
-    //  logger.info("active: {} cancel: {} completed: {}",job.isActive,job.isCancelled,job.isCompleted)
-    runBlocking { delay(5000) }
-    logger.info("After delay")
-    // logger.info("active: {} cancel: {} completed: {}",job.isActive,job.isCancelled,job.isCompleted)
+  // onConnection()
+  //val job =tester2()
+  logger.info("after")
+  //  logger.info("active: {} cancel: {} completed: {}",job.isActive,job.isCancelled,job.isCompleted)
+  runBlocking { delay(5000) }
+  logger.info("After delay")
+  // logger.info("active: {} cancel: {} completed: {}",job.isActive,job.isCancelled,job.isCompleted)
 }
 
 fun onConnection() {
-    logger.info("On COnnection called")
-    scopey.launch {
-        logger.info(" in launch.. delay")
-        delay(1000)
-        logger.info(" in launch.. after ")
-        for (x in 1..10) {
-            launch {
-                withTimeout(1500L) {
-                    logger.info(" in withTImeout ")
-                    logger.info("loop: {}", this.coroutineContext.toString())
-                }
-            }
+  logger.info("On COnnection called")
+  scopey.launch {
+    logger.info(" in launch.. delay")
+    delay(1000)
+    logger.info(" in launch.. after ")
+    for (x in 1..10) {
+      launch {
+        withTimeout(1500L) {
+          logger.info(" in withTImeout ")
+          logger.info("loop: {}", this.coroutineContext.toString())
         }
+      }
     }
+  }
 }
 
 suspend fun tester() {
-    logger.info("Tester 2 called")
-    coroutineScope {
-        logger.info(" in launch")
-        withTimeout(1500L) {
-            logger.info(" withTimeout ")
-            delay(1000)
-            for (x in 1..10) {
-                logger.info("loop: {}", this.coroutineContext.toString())
-            }
-        }
+  logger.info("Tester 2 called")
+  coroutineScope {
+    logger.info(" in launch")
+    withTimeout(1500L) {
+      logger.info(" withTimeout ")
+      delay(1000)
+      for (x in 1..10) {
+        logger.info("loop: {}", this.coroutineContext.toString())
+      }
     }
+  }
 }
 
 fun tester2() {
-    logger.info("On COnnection called")
+  logger.info("On COnnection called")
 
-    scopey.launch {
-        logger.info(" in launch.. delay")
-        delay(1000)
-        logger.info(" in launch.. after ")
-        for (x in 1..10) {
-            launch {
-                withTimeout(1500L) {
-                    logger.info(" in withTImeout ")
-                    logger.info("loop: {}", this.coroutineContext.toString())
-                }
-            }
+  scopey.launch {
+    logger.info(" in launch.. delay")
+    delay(1000)
+    logger.info(" in launch.. after ")
+    for (x in 1..10) {
+      launch {
+        withTimeout(1500L) {
+          logger.info(" in withTImeout ")
+          logger.info("loop: {}", this.coroutineContext.toString())
         }
+      }
     }
+  }
 }
--- src/main/kotlin/konem/protocol/websocket/KoneMessageReceiver.kt
+++ src/main/kotlin/konem/protocol/websocket/KoneMessageReceiver.kt
@@ -1,45 +1,161 @@
 package konem.protocol.websocket
 
 
-import com.squareup.moshi.JsonClass
-import com.squareup.moshi.Moshi
+import com.squareup.moshi.*
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import konem.netty.stream.ReceiverHandler
 import org.slf4j.LoggerFactory
 import java.net.InetSocketAddress
+import java.util.*
 
 
 @JsonClass(generateAdapter = true)
-data class KoneMesssage(val type: String, val message: String)
+data class KoneMesssage(val type: String) {
 
+  constructor(type: String, message: Any) : this(type)
 
-data class KoneHeartbeat private constructor(val type: String) {
-    constructor() : this(heartbeat)
-
-    companion object {
-        const val heartbeat = "heartbeat"
+  companion object {
+    private const val heartbeat = "heartbeat"
+    fun KoneHeartbeat(): KoneMesssage {
+      return KoneMesssage(heartbeat, Date().toString())
     }
+  }
+}
+
+enum class KonemTypes {
+  @Json(name = "heartbeat")
+  HEARTBEAT,
+  @Json(name = "status")
+  STATUS,
+  @Json(name = "unknown")
+  UNKNOWN
+}
+
+class MainMessageAdaptor {
+
+  val typeOption = JsonReader.Options.of("type","heartbeat","status","unknown")
+  val heartOpt = JsonReader.Options.of("message","sent")
+
+  @FromJson
+  fun fromJson(jsonReader: JsonReader): MainMessage {
+
+    jsonReader.beginObject()
+    if(jsonReader.selectName(typeOption) == 0){
+      val type = jsonReader.selectString(typeOption)
+      if(type == 1){
+        val ttt = jsonReader.selectName(heartOpt)
+        if(ttt == 0 ){
+          jsonReader.beginObject()
+          jsonReader.selectName(heartOpt)
+          val msg = jsonReader.nextString()
+
+          jsonReader.endObject()
+          jsonReader.endObject()
+          return   MainMessage(KonemTypes.HEARTBEAT, KonemHeartbeat(msg))
+        }
+      }
+
+    }
+
+
+    return MainMessage(KonemTypes.UNKNOWN, KonemTypes.UNKNOWN.toString())
+  }
+
+
+  private fun extract(name: String, reader: JsonReader): String {
+    var extracted = ""
+    while (reader.hasNext() &&(reader.peek() != JsonReader.Token.END_DOCUMENT) ) {
+
+      if (reader.peek() == JsonReader.Token.NAME) {
+        val check = reader.nextName()
+        if (check == name) {
+          extracted = reader.nextString()
+        }
+        else{
+          reader.skipValue()
+        }
+      }
+      println("XXX $reader EXTRACTED: [ $extracted ]")
+
+    }
+
+    return extracted
+  }
+
+  @ToJson
+  fun toJson(message: MainMessage) = when (message.type) {
+    KonemTypes.HEARTBEAT -> {
+      message
+    }
+    KonemTypes.STATUS -> {
+      message
+    }
+    else -> {
+      MainMessage(KonemTypes.UNKNOWN, message.message)
+    }
+  }
+
+  companion object {
+    const val heartbeat: String = "heartbeat"
+  }
 }
 
 
-class KoneMessageReceiver(private val receive: (InetSocketAddress, KoneMesssage) -> Unit) : ReceiverHandler<String>() {
-    private val logger = LoggerFactory.getLogger(KoneMessageReceiver::class.java)
-    private val adaptor = moshi.adapter(KoneMesssage::class.java)
+//@JsonClass(generateAdapter = true)
+data class MainMessage(val type: KonemTypes, val message: Any) {
 
-    override fun read(addr: InetSocketAddress, message: String) {
-        try {
-            val kMessage = adaptor.fromJson(message)
-            if (kMessage != null) {
-                receive(addr, kMessage)
-            }
-        } catch (e: Exception) {
-            logger.error("exception in casting of message : {} ", e.message)
-        }
-
+  companion object {
+    fun Heartbeat(): MainMessage {
+      return MainMessage(KonemTypes.HEARTBEAT, KonemHeartbeat(Date().toString()))
     }
 
-    companion object {
-        val moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    fun Status(
+      shortName: String,
+      errors: Int,
+      received: Int,
+      sent: Int,
+      description: String
+    ): MainMessage {
+      return MainMessage(
+        KonemTypes.STATUS,
+        KonemStatus(shortName, errors, received, sent, description)
+      )
     }
+  }
+}
+
+@JsonClass(generateAdapter = true)
+data class KonemHeartbeat(val sent: String)
+
+@JsonClass(generateAdapter = true)
+data class KonemStatus(
+  val shortName: String,
+  val errors: Int,
+  val received: Int,
+  val sent: Int,
+  val description: String
+)
+
+
+class KoneMessageReceiver(private val receive: (InetSocketAddress, KoneMesssage) -> Unit) :
+  ReceiverHandler<String>() {
+  private val logger = LoggerFactory.getLogger(KoneMessageReceiver::class.java)
+  private val adaptor = moshi.adapter(KoneMesssage::class.java)
+
+  override fun read(addr: InetSocketAddress, message: String) {
+    try {
+      val kMessage = adaptor.fromJson(message)
+      if (kMessage != null) {
+        receive(addr, kMessage)
+      }
+    } catch (e: Exception) {
+      logger.error("exception in casting of message : {} ", e.message)
+    }
+
+  }
+
+  companion object {
+    val moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+  }
 
 }
