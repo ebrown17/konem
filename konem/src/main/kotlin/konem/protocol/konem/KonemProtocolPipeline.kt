53a86910c180de771620fa80f5d25548329af5e4
ebrown <ebrown7871@gmail.com>
2026-01-11 14:59:35 -0800

    fixed string encoder and decoder that I accidentally deleted

--- konem/src/main/kotlin/konem/protocol/konem/KonemProtocolPipeline.kt
+++ konem/src/main/kotlin/konem/protocol/konem/KonemProtocolPipeline.kt
@@ -8,14 +8,11 @@ import io.netty.util.CharsetUtil
 import konem.netty.ProtocolPipeline
 import konem.protocol.konem.json.KonemJsonDecoder
 import konem.protocol.konem.json.KonemJsonEncoder
-import konem.protocol.konem.json.KonemJsonMessageHandler
 import konem.protocol.konem.json.WebSocketFrameJsonDecoder
 import konem.protocol.konem.json.WebSocketFrameJsonEncoder
-import konem.protocol.konem.string.KonemStringMessageHandler
 import konem.protocol.konem.string.WebSocketFrameStringDecoder
 import konem.protocol.konem.string.WebSocketFrameStringEncoder
 import konem.protocol.konem.wire.KonemWireCodec
-import konem.protocol.konem.wire.KonemWireMessageHandler
 import konem.protocol.konem.wire.WebSocketFrameWireDecoder
 import konem.protocol.konem.wire.WebSocketFrameWireEncoder
 
@@ -23,12 +20,11 @@ class KonemProtocolPipeline private constructor(){
     companion object {
         fun getKonemJsonPipeline(): ProtocolPipeline<konem.data.json.KonemMessage> {
             return ProtocolPipeline(
-                protocolMessageHandler = {
-                    Pair("messageHandler", KonemJsonMessageHandler())
-                },
                 protoPipelineCodecs = { pipeline ->
-                    pipeline["konemEncoder"] = KonemJsonEncoder()
+                    pipeline["stringDecoder"] = StringDecoder(CharsetUtil.UTF_8)
                     pipeline["konemDecoder"] = KonemJsonDecoder()
+                    pipeline["stringEncoder"] = StringEncoder(CharsetUtil.UTF_8)
+                    pipeline["konemEncoder"] = KonemJsonEncoder()
                 },
                 wsPipelineFrameCodec = { pipeline ->
                     pipeline["webSocketFrameEncoder"] = WebSocketFrameJsonEncoder()
@@ -38,9 +34,7 @@ class KonemProtocolPipeline private constructor(){
 
         fun getKonemWirePipeline(): ProtocolPipeline<konem.data.protobuf.KonemMessage> {
             return ProtocolPipeline(
-                protocolMessageHandler = {
-                    Pair("messageHandler", KonemWireMessageHandler())
-                },
+
                 protoPipelineCodecs = { pipeline ->
                     pipeline["frameDecoder"] = ProtobufVarint32FrameDecoder()
                     pipeline["frameEncoder"] = ProtobufVarint32LengthFieldPrepender()
@@ -55,9 +49,6 @@ class KonemProtocolPipeline private constructor(){
 
         fun getKonemStringPipeline(): ProtocolPipeline<String> {
             return ProtocolPipeline(
-                protocolMessageHandler = {
-                    Pair("messageHandler", KonemStringMessageHandler())
-                },
                 protoPipelineCodecs = { pipeline ->
                     pipeline["stringDecoder"] = StringDecoder(CharsetUtil.UTF_8)
                     pipeline["stringEncoder"] = StringEncoder(CharsetUtil.UTF_8)
